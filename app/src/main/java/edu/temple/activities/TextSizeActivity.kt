c1a90914f355dca5d4086b41e3d783329c47b5f7
Eric Reizas <tuo17224@temple.edu>
2024-02-22 19:41:30 -0500

    Referenced given size instead of size as index of the array

--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -24,7 +24,7 @@ class TextSizeActivity : AppCompatActivity() {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
             adapter = TextSizeAdapter(textSizes){
-                setResult(RESULT_OK,Intent(this@TextSizeActivity,DisplayActivity::class.java).putExtra(textSizeKey,textSizes[it]))
+                setResult(RESULT_OK,Intent(this@TextSizeActivity,DisplayActivity::class.java).putExtra(textSizeKey,it))
                 finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
